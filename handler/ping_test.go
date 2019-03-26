b2e98e3f6804e33018697872472b7ec4062ae4e1
Mahendra Kariya <mahendra.kariya@go-jek.com>
2019-03-26 16:03:12 +0530

    Add Ping handler
    Co-authored-by: Himani Agrawal <himani93@gmail.com>

---
+++ handler/ping_test.go
@@ -0,0 +1,20 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPingHandler(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/ping", nil)
+
+	PingHandler(w, r)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "application/json", w.Header().Get("Content-Type"))
+	assert.Equal(t, "{\"success\": \"pong\"}", w.Body.String())
+}
--- server/router.go
+++ server/router.go
@@ -2,10 +2,12 @@ package server
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/mahakamcloud/netd/handler"
 	"github.com/mahakamcloud/netd/service"
 )
 
 func Router(services *service.Services) *mux.Router {
 	router := mux.NewRouter()
+	router.HandleFunc("/ping", handler.PingHandler).Methods("GET")
 	return router
 }

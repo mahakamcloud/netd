0d1f8e4c36c87c5d86666ca160aaa43746c5f084
Mahendra Kariya <mahendra.kariya@go-jek.com>
2019-03-26 17:26:40 +0530

    Add constructor for host
    Co-authored-by: Himani Agrawal <himani93@gmail.com>

--- netd/host/host.go
+++ netd/host/host.go
@@ -1,10 +1,21 @@
 package host
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
 type Host struct {
 	name   string `json:"name"`
 	ipAddr net.IP `json:"ip_addr"`
+}
+
+func New(name, ipAddr string) (*Host, error) {
+	ip := net.ParseIP(ipAddr)
+	if ip == nil {
+		return nil, fmt.Errorf("IP Address %v is invalid for host %v", ipAddr, name)
+	}
+	return &Host{name, ip}, nil
 }
 
 func (h *Host) Name() string {
---
+++ netd/host/host_test.go
@@ -0,0 +1,22 @@
+package host
+
+import (
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHostIsCreated(t *testing.T) {
+	h, _ := New("localhost", "127.0.0.1")
+
+	assert.Equal(t, h.name, "localhost")
+	assert.Equal(t, h.ipAddr, net.IPv4(127, 0, 0, 1))
+}
+
+func TestHostCreationFailsForInvalidIp(t *testing.T) {
+	_, err := New("localhost", "266.266.266.266")
+
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), "IP Address 266.266.266.266 is invalid for host localhost")
+}
